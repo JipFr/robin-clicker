bb58c17b1466e88322c7751c71a9c7ffff384c66
Jip Fr <jipfrijlink@gmail.com>
2021-01-05 21:35:05 +0100

    Aanpassingen

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // Init
 let points = 0;
 let highscore = Number(localStorage.getItem("highscore")) || 0
+const maxAge = 40;
+let minSpeed = 5;
 
 // Draw
 const canvas = document.querySelector("canvas");
@@ -25,16 +27,20 @@ for(let source of sources) {
 	images.push(img);	
 }
 
+// Particles
+let particles = [];
+
 // Make heads
-const heads = [];
-for(let i = 0; i < 50; i++) {
+let heads = [];
+
+function newHead() {
 	heads.push({
 		x: Math.random() * canvas.width,
 		y: Math.random() * canvas.height,
 		image: images[Math.floor(Math.random() * images.length)],
 		direction: Math.random() * (Math.PI*2),
 		rotation: Math.random() * (Math.PI*2),
-		speed: Math.floor(Math.random() * 2) + 5
+		speed: Math.floor(Math.random() * 2) + minSpeed
 	});	
 }
 
@@ -50,6 +56,20 @@ function draw() {
 		ctx.translate(head.x, head.y);
 		ctx.rotate(head.rotation)
 		ctx.drawImage(head.image, -50, -head.image.height / 2)
+
+		ctx.restore();
+	}
+
+	particles = particles.filter(particle => maxAge > particle.age)
+	for(let particle of particles) {
+		ctx.save();
+
+		ctx.translate(particle.x, particle.y);
+		ctx.rotate(particle.rotation);
+
+		ctx.globalAlpha = Math.max(1 - (particle.age / maxAge), 0);
+		ctx.strokeStyle = "orange";
+		ctx.strokeRect(-8, -8, 16, 16);
 
 		ctx.restore();
 	}
@@ -79,6 +99,17 @@ function update() {
 		}
 
 	}
+
+	for(let particle of particles) {
+		const newX = particle.speed * Math.cos(particle.direction);
+		const newY = particle.speed * Math.sin(particle.direction);
+		
+		particle.x += newX;
+		particle.y += newY;
+
+		particle.age++
+	}
+
 }
 
 function main() {
@@ -87,6 +118,11 @@ function main() {
 }
 
 function init() {
+
+	heads = [];
+	for(let i = 0; i < 50; i++) {
+		newHead();
+	}
 
 	canvas.addEventListener("click", evt => {
 		const { top, left } = canvas.getBoundingClientRect()
@@ -101,13 +137,50 @@ function init() {
 		if(sum > 0) {
 			// Success
 			points++
+
+			for(let i = 0; i < Math.floor(Math.random() * 100) + 100; i++) {
+				particles.push({
+					x,
+					y,
+					rotation: Math.random() * (Math.PI*2),
+					direction: Math.random() * (Math.PI*2),
+					speed: Math.random() * 5 + 1,
+					age: 0
+				});
+
+			}
+			
+			const closest = heads.map(h => {
+				let difX = Math.abs(h.x - x);
+				let difY = Math.abs(h.y - y);
+				return {
+					head: h,
+					distance: Math.sqrt(difX * difX + difY * difY)
+				}
+			}).sort((a, b) => a.distance - b.distance)[0].head;
+			
+			heads = heads.filter(v => v !== closest);
+
+			minSpeed++
+			newHead();
+
 		} else {
 			// Fail
+			
+			minSpeed = 5;
+			
 			points = 0;
+
 			document.querySelector("canvas").classList.add("shake");
 			setTimeout(() => {
 				document.querySelector("canvas").classList.remove("shake");
 			}, 500);
+
+			heads = [];
+			for(let i = 0; i < 50; i++) {
+				newHead();
+			}
+			
 		}
 		console.log(sum);
 
