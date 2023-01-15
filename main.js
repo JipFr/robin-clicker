45864f0dab445e919fac3e6a36a118751bd46b3d
Jip Fr <jipfrijlink@gmail.com>
2023-01-15 14:44:09 +0100

    Add emoji

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 // Init
 let points = 0;
-let highscore = Number(localStorage.getItem("highscore")) || 0
+let highscore = Number(localStorage.getItem("highscore")) || 0;
 const maxAge = 40;
 let minSpeed = 5;
 let doInit = true;
@@ -14,18 +13,18 @@ canvas.width = canvas.scrollWidth;
 canvas.height = canvas.scrollHeight;
 
 // Generate sources
-const imageCount = 20;
+const imageCount = 21;
 const sources = [];
-for(let i = 1; i <= imageCount; i++) {
+for (let i = 1; i <= imageCount; i++) {
 	sources.push(`hoofden/${i}.png`);
 }
 
 // Get images
 const images = [];
-for(let source of sources) {
+for (let source of sources) {
 	const img = new Image();
 	img.src = source;
-	images.push(img);	
+	images.push(img);
 }
 
 // Particles
@@ -39,46 +38,44 @@ function newHead() {
 		x: Math.floor(Math.random() * 2) === 0 ? -200 : canvas.width + 200,
 		y: Math.random() * canvas.height,
 		image: images[Math.floor(Math.random() * images.length)],
-		direction: Math.random() * (Math.PI*2),
-		rotation: Math.random() * (Math.PI*2),
-		speed: Math.floor(Math.random() * 2) + minSpeed
-	});	
+		direction: Math.random() * (Math.PI * 2),
+		rotation: Math.random() * (Math.PI * 2),
+		speed: Math.floor(Math.random() * 2) + minSpeed,
+	});
 }
-
 
 function draw() {
 	canvas.width = canvas.scrollWidth;
 	canvas.height = canvas.scrollHeight;
 	// ctx.drawImage(images[0], 100, 100);
 
-	for(let head of heads) {
+	for (let head of heads) {
 		ctx.save();
 
 		ctx.translate(head.x, head.y);
-		ctx.rotate(head.rotation)
-		ctx.drawImage(head.image, -50, -head.image.height / 2)
+		ctx.rotate(head.rotation);
+		ctx.drawImage(head.image, -50, -head.image.height / 2);
 
 		ctx.restore();
 	}
 
-	particles = particles.filter(particle => maxAge > particle.age)
-	for(let particle of particles) {
+	particles = particles.filter((particle) => maxAge > particle.age);
+	for (let particle of particles) {
 		ctx.save();
 
 		ctx.translate(particle.x, particle.y);
 		ctx.rotate(particle.rotation);
 
-		ctx.globalAlpha = Math.max(1 - (particle.age / maxAge), 0);
+		ctx.globalAlpha = Math.max(1 - particle.age / maxAge, 0);
 		ctx.strokeStyle = "orange";
 		ctx.strokeRect(-8, -8, 16, 16);
 
 		ctx.restore();
 	}
-
 }
 
 function update() {
-	for(let head of heads) {
+	for (let head of heads) {
 		const newX = head.speed * Math.cos(head.direction);
 		const newY = head.speed * Math.sin(head.direction);
 
@@ -87,30 +84,28 @@ function update() {
 
 		head.rotation += 0.01;
 
-		if(head.x > canvas.width + 200) {
-			head.x = -200
-		} else if(head.x < -200) {
-			head.x = canvas.width + 200
+		if (head.x > canvas.width + 200) {
+			head.x = -200;
+		} else if (head.x < -200) {
+			head.x = canvas.width + 200;
 		}
 
-		if(head.y > canvas.height + 200) {
-			head.y = -200
-		} else if(head.y < -200) {
-			head.y = canvas.height + 200
+		if (head.y > canvas.height + 200) {
+			head.y = -200;
+		} else if (head.y < -200) {
+			head.y = canvas.height + 200;
 		}
-
 	}
 
-	for(let particle of particles) {
+	for (let particle of particles) {
 		const newX = particle.speed * Math.cos(particle.direction);
 		const newY = particle.speed * Math.sin(particle.direction);
-		
+
 		particle.x += newX;
 		particle.y += newY;
 
-		particle.age++
+		particle.age++;
 	}
-
 }
 
 function main() {
@@ -119,57 +114,56 @@ function main() {
 }
 
 function init() {
-
 	heads = [];
-	for(let i = 0; i < 50; i++) {
+	for (let i = 0; i < 50; i++) {
 		newHead();
 	}
 
-	canvas.addEventListener("click", evt => {
-		const { top, left } = canvas.getBoundingClientRect()
+	canvas.addEventListener("click", (evt) => {
+		const { top, left } = canvas.getBoundingClientRect();
 		const x = evt.clientX - left;
 		const y = evt.clientY - top;
-		
+
 		const { data } = ctx.getImageData(x, y, 1, 1);
 		let sum = 0;
-		for(let n of data) {
+		for (let n of data) {
 			sum += n;
 		}
-		if(sum > 0) {
+		if (sum > 0) {
 			// Success
-			points++
+			points++;
 
-			for(let i = 0; i < Math.floor(Math.random() * 100) + 100; i++) {
+			for (let i = 0; i < Math.floor(Math.random() * 100) + 100; i++) {
 				particles.push({
 					x,
 					y,
-					rotation: Math.random() * (Math.PI*2),
-					direction: Math.random() * (Math.PI*2),
+					rotation: Math.random() * (Math.PI * 2),
+					direction: Math.random() * (Math.PI * 2),
 					speed: Math.random() * 5 + 1,
-					age: 0
+					age: 0,
 				});
-
 			}
-			
-			const closest = heads.map(h => {
-				let difX = Math.abs(h.x - x);
-				let difY = Math.abs(h.y - y);
-				return {
-					head: h,
-					distance: Math.sqrt(difX * difX + difY * difY)
-				}
-			}).sort((a, b) => a.distance - b.distance)[0].head;
-			
-			heads = heads.filter(v => v !== closest);
+
+			const closest = heads
+				.map((h) => {
+					let difX = Math.abs(h.x - x);
+					let difY = Math.abs(h.y - y);
+					return {
+						head: h,
+						distance: Math.sqrt(difX * difX + difY * difY),
+					};
+				})
+				.sort((a, b) => a.distance - b.distance)[0].head;
+
+			heads = heads.filter((v) => v !== closest);
 
 			minSpeed += 0.1;
 			newHead();
-
 		} else {
 			// Fail
-			
+
 			minSpeed = 5;
-			
+
 			points = 0;
 
 			document.querySelector("canvas").classList.add("shake");
@@ -178,21 +172,19 @@ function init() {
 			}, 500);
 
 			heads = [];
-			for(let i = 0; i < 50; i++) {
+			for (let i = 0; i < 50; i++) {
 				newHead();
 			}
-
 		}
 		console.log(sum);
 
 		updatePoints();
-
 	});
 
-	if(doInit) {
+	if (doInit) {
 		doInit = false;
 	}
-	
+
 	main();
 	setInterval(main, 1e3 / 60);
 	updatePoints();
@@ -200,10 +192,14 @@ function init() {
 init();
 
 function updatePoints() {
-	if(points > highscore) {
+	if (points > highscore) {
 		highscore = points;
 		localStorage.setItem("highscore", highscore);
 	}
-	document.querySelector(".points").innerText = `${points} punt${points !== 1 ? 'en' : ''}`
-	document.querySelector(".highscore").innerText = `${highscore} punt${highscore !== 1 ? 'en' : ''}`
+	document.querySelector(".points").innerText = `${points} punt${
+		points !== 1 ? "en" : ""
+	}`;
+	document.querySelector(".highscore").innerText = `${highscore} punt${
+		highscore !== 1 ? "en" : ""
+	}`;
 }
